a95ec2d0eeb36d5451b93a8045b1988f6ff81bbf
William Huebner <wilhueb@gmail.com>
2018-12-06 20:50:33 -0500

    added parsing of items

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+import os
+import re
 from time import sleep
 
 from selenium import webdriver
@@ -5,7 +7,72 @@ from selenium import webdriver
 import login
 
 
-def send_keys_slow(elem, text):
+class LotsBot():
+    def __init__(self, driver: webdriver.Chrome) -> None:
+        self.driver = driver
+
+        self.seen_items = []
+
+    def run(self, item_count: int) -> None:
+        """parse individual items tab (https://beta.888lots.com/catalog)
+        and add every item to the cart
+        """
+
+        self.driver.refresh()
+        
+        # scroll to bottom of page to load more items
+        #try:
+        #    self.driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+        
+        #    sleep(10)
+        #except:
+        #    pass
+        
+        table = self.driver.find_elements_by_xpath('//div[@class="row products_page"]/div/ul/li')
+
+        for i in range(item_count):
+            item = table[i]
+
+            sku = re.search('B\d.{8}', item.text).group()
+
+            if sku in self.seen_items:
+                continue
+
+            print('processing: SKU ' + sku)
+
+            #if item contains div with class containing 'alert':
+
+            if 'not order it' in item.text:
+                print(' ~ beep beep boop boop the item is already in someone\'s cart')
+
+                continue
+
+            #buy_now =
+            
+            #buy_now.click()
+            
+            #add_to_cart =
+
+            #add_to_cart.click()
+
+            self.seen_items.append(sku)
+        
+        # check for runtime, optimize so it does not run constantly but fast enough
+
+
+    def run_loop(self, item_count: int=20) -> None:
+        while True:
+            if not os.path.isfile('run'):
+                print('run file does not exist, sleeping bot for 10 seconds...')
+                
+                sleep(10)
+            
+            self.run(item_count)
+
+            sleep(10)
+
+
+def send_keys_slow(elem: webdriver.remote.webelement.WebElement, text: str) -> None:
     for c in text:
         elem.send_keys(c)
 
@@ -47,6 +114,10 @@ def main() -> None:
         end_tour.click()
 
     driver.get('https://beta.888lots.com/catalog')
+    
+    bot = LotsBot(driver)
+
+    bot.run_loop()
 
 
 if __name__ == '__main__':
